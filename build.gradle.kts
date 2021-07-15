cb93c973bc7410ec8a2e8238c7cee2128d8c512e
Pierre Ernst <pierre@DisplayLink.lan>
2021-07-15 16:52:52 -0400

    Fixed release number

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,12 +8,12 @@ repositories {
 
 dependencies {
     implementation("org.kohsuke:github-api:1.131")
-	implementation("com.squareup.okhttp3:okhttp:3.14.9")
+    implementation("com.squareup.okhttp3:okhttp:3.14.9")
     implementation("com.github.package-url:packageurl-java:1.3.1")
     
     testImplementation("org.junit.jupiter:junit-jupiter:5.7.2")
 }
 
 group = "fr.gnodet.githubfs"
-version = "1.0.0-PERNST"
+version = "0.0.1-PERNST"
 description = "GitHub File System"
